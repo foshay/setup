97ccba3106a72672dc7a5dd3dd1e824933632e00
foshay <foshayi@oregonstate.edu>
2020-05-04 21:14:09 -0700

    wip

--- init.sh
+++ init.sh
@@ -34,6 +34,7 @@ packagelist=(
 	libssl-dev
 	python3
 	python3-pip
+    python3-setuptools
 	wireshark
 	ruby
 	gdb
@@ -45,6 +46,7 @@ packagelist=(
 	libstdc++6:i386
 	lib32z1
 	openjdk-8-jre
+    openjdk-8-jdk
     tmux
     ubuntu-make
 	vim
@@ -52,10 +54,13 @@ packagelist=(
 	yasm
 	pkg-config
 	libgmp-dev
+    libmp3-dev
+    libmpc-dev
 	libpcap-dev
 	libz2-dev
 	libnss-dev
 	libkrb5-dev
+    qtdeclarative5-dev
 )
 log "Installing new packages via apt"
 sudo -E apt-get install -y ${packagelist[@]}
@@ -63,6 +68,7 @@ sudo -E apt-get install -y ${packagelist[@]}
 cp .tmux.conf ~/
 cp -r .vim ~/
 cp .vimrc ~/
+git config --global core.editor vim
 
 log "Upgrading pip"
 pip3 install --upgrade pip
@@ -98,9 +104,12 @@ chmod +x setup.sh
 ./setup.sh
 sudo snap install john-the-ripper
 sudo snap install --classic code
+cd ~/
 mkdir CTF
-git clone https://github.com/zardus/ctf-tools.git ~/CTF
-git config --global core.editor vim
+cd ~/CTF
+git clone https://github.com/zardus/ctf-tools.git
+git clone https://github.com/Ganapati/RsaCtfTool.git
+pip3 install -r "RsaCtfTool/requirements.txt"
 mkdir Applications
 echo ---
 cat ~/.ssh/id_rsa.pub
